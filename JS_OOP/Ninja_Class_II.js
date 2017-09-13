51c1c8a24caefd3f73b09f0dd831490c517f0d4e
kevinpav <pavdog@gmail.com>
2017-09-13 16:26:03 -0400

    Moved .punch .kick to prototypes

--- JS_OOP/Ninja_Class_II.js
+++ JS_OOP/Ninja_Class_II.js
@@ -2,51 +2,43 @@
 
 // Private variables are scoped to the constructor with the var keyword
 function Ninja(name){
- // if ( !(this instanceof Ninja)) {
-   // return new Ninja(name);
-  //}
 
+  this.name = name;
+  this.health = 100;
+  let speed = 3;
+  let strength = 3;
   console.log(this)
-    this.name = name;
-    this.health = 100;
-    let speed = 3;
-    let strength = 3;
-    console.log(this)
-    // Getter function
-    this.sayName = function(){
-        console.log("My ninja name is " + this.name);
-    };
-    
-    // 'Getter' functions help us read private variables
-    this.showStats = function(){
-      console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
-    }
+  // Getter function
+  this.sayName = function(){
+      console.log("My ninja name is " + this.name);
+  };
 
-    this.drinkSake = function(){
-      this.health += 10;
-    }
+  // 'Getter' functions help us read private variables
+  this.showStats = function(){
+    console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + speed + ", Strength: " + strength);
+  }
 
-    function decreaseHealth(amt){
-
-    }
-
-    this.punch = function(opponent){
-      if ( !(opponent instanceof Ninja)) {
-        console.log("Error: .punch() was passed a non-Ninja object!")
-      }
-
-      opponent.health -= 5;
-      console.log(this.name + " punches " + opponent.name);
-    }
-
-    this.kick = function(opponent){
-      if ( !(opponent instanceof Ninja)) {
-        console.log("Error: .kick() was passed a non-Ninja object!")
-      }
-      opponent.health -= 15;
-      console.log(this.name + " kicks " + opponent.name);
-    }
+  this.drinkSake = function(){
+    this.health += 10;
+  }
 }
+
+Ninja.prototype.punch = function(opponent){
+  if ( !(opponent instanceof Ninja)) {
+    console.log("Error: .punch() was passed a non-Ninja object!")
+  }
+  opponent.health -= 5;
+  console.log(this.name + " punches " + opponent.name);
+}
+
+Ninja.prototype.kick = function(opponent){
+  if ( !(opponent instanceof Ninja)) {
+    console.log("Error: .kick() was passed a non-Ninja object!")
+  }
+  opponent.health -= 15;
+  console.log(this.name + " kicks " + opponent.name);
+}
+
 
 // var ninja1 = new Ninja("Hyabusa");
 // ninja1.sayName();
